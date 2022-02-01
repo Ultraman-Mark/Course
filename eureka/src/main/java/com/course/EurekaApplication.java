52ecc3af02c523544abf849a57e340c519f892cb
Pengyunlu <2278625687@qq.com>
2022-02-01 19:38:08 +0800

    “system注册到eureka”

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -22,7 +22,7 @@ public class EurekaApplication {
 		SpringApplication app = new SpringApplication(EurekaApplication.class);
 		Environment env = app.run(args).getEnvironment();
 		LOG.info("启动成功!!");
-		LOG.info("Eureka地址:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+		LOG.info("System地址:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }

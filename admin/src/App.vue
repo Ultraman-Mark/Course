2cf988c346c374717684ec03072ddcf9c4b12c16
Pengyunlu <2278625687@qq.com>
2022-04-03 09:19:22 +0800

    4-2.集成vue,bootstrap,简化登录界面

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -4,23 +4,8 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  $('body').attr('class', 'login-layout light-login');
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

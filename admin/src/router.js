8e231fb7d765abf4f7adef274d12f58222ec3551
Pengyunlu <2278625687@qq.com>
2022-04-05 13:02:30 +0800

    4-3 1.增加login页面 2.增加路由配置，访问/login时，加载login组件 3.增加login页面登录时，跳转admin页面

---
+++ admin/src/router.js
@@ -0,0 +1,40 @@
+import Vue from "vue"
+import { createRouter, createWebHistory } from 'vue-router';//和vue2 路由不同
+import Login from "./views/login.vue"
+import Admin from  "./views/admin.vue"
+
+const routerHistory = createWebHistory();
+
+const router = createRouter({
+    history: routerHistory,//history写法不同
+    routes: [
+        {
+            path:'/login',
+            component: Login,
+        },{
+            path:'/admin',
+            component:Admin,
+        }
+    ],
+});
+
+export default router;
+
+// Vue.use(Router);
+//
+// export default new Router({
+//     mode: 'history',
+//     base: process.env.BASE_URL,
+//     routes: [{
+//         path: '*',
+//         redirect: "/login",
+//     }, {
+//         path: '/login',
+//         component: Login
+//     }]
+// })
+
+
+
+
+

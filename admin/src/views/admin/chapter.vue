ac5e31213c61b7c8f5d16635aeb99225c37eeb67
Pengyunlu <2278625687@qq.com>
2022-04-10 17:14:20 +0800

    5-5.增加刷新,增加分页组件pagination.vue

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,79 +1,93 @@
 <template>
-  <table id="simple-table" class="table  table-bordered table-hover">
-          <thead>
-          <tr>
-            <th>ID</th>
-            <th>名称</th>
-            <th>课程ID</th>
-            <th>操作</th>
-          </tr>
-          </thead>
+  <div>
+    <p>
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+      </button>
+    </p>
 
-          <tbody>
-          <tr v-for="chapter in chapters">
-            <td>{{chapter.id}}</td>
-            <td>{{chapter.name}}</td>
-            <td>{{chapter.courseId}}</td>
-            <td>
-              <div class="hidden-sm hidden-xs btn-group">
-                <button class="btn btn-xs btn-success">
-                  <i class="ace-icon fa fa-check bigger-120"></i>
-                </button>
+    <pagination ref="pagination" v-bind:list="list"></pagination>
 
-                <button class="btn btn-xs btn-info">
-                  <i class="ace-icon fa fa-pencil bigger-120"></i>
-                </button>
+    <table id="simple-table" class="table  table-bordered table-hover">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>课程ID</th>
+        <th>操作</th>
+      </tr>
+      </thead>
 
-                <button class="btn btn-xs btn-danger">
-                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                </button>
+      <tbody>
+      <tr v-for="chapter in chapters">
+        <td>{{chapter.id}}</td>
+        <td>{{chapter.name}}</td>
+        <td>{{chapter.courseId}}</td>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button class="btn btn-xs btn-success">
+              <i class="ace-icon fa fa-check bigger-120"></i>
+            </button>
 
-                <button class="btn btn-xs btn-warning">
-                  <i class="ace-icon fa fa-flag bigger-120"></i>
-                </button>
-              </div>
+            <button class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
 
-              <div class="hidden-md hidden-lg">
-                <div class="inline pos-rel">
-                  <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                    <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                  </button>
+            <button class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
 
-                  <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                    <li>
-                      <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+            <button class="btn btn-xs btn-warning">
+              <i class="ace-icon fa fa-flag bigger-120"></i>
+            </button>
+          </div>
+
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+              </button>
+
+              <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
 																			<span class="blue">
 																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
 																			</span>
-                      </a>
-                    </li>
+                  </a>
+                </li>
 
-                    <li>
-                      <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
 																			<span class="green">
 																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
 																			</span>
-                      </a>
-                    </li>
+                  </a>
+                </li>
 
-                    <li>
-                      <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
 																			<span class="red">
 																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
 																			</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </td>
-          </tr>
-          </tbody>
-        </table>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+
 </template>
 
 <script>
+import Pagination from "../../components/pagination"
 export default {
+  components:{Pagination},
   name: "chapter",
   data: function (){
     return {
@@ -82,19 +96,22 @@ export default {
   },
   mounted: function (){
     let _this = this;
-    _this.list();
+    _this.$refs.pagination.size = 5;
+    _this.list(1);
     //sidebar激活样式方法一
     // this.$parent.$parent.activeSidebar("business-chapter-sidebar");
   },
   methods:{
-    list(){
+    list(page){
       let _this = this;
+
       _this.$axios.post('http://127.0.0.1:9000/business/admin/chapter/list',{
-        page:1,
-        size:1
+        page: page,
+        size: _this.$refs.pagination.size,
       }).then((response)=>{
-        console.log("查询大章列表结果:",response.data.list);
+        console.log("查询大章列表结果:",response);
         _this.chapters = response.data.list;
+        _this.$refs.pagination.render(page,response.data.total);
       })
     }
   }

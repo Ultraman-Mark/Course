4380f7d05b6908e389797ad2f6a52625fc3641a9
Pengyunlu <2278625687@qq.com>
2022-04-10 19:44:29 +0800

    5-6.增加 新增 功能, 增加工具类CopyUtil

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <p>
+      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-edit"></i>
+        新增
+      </button>
+      &nbsp;
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
@@ -80,6 +85,37 @@
       </tr>
       </tbody>
     </table>
+
+    <div class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">表单</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <label class="col-sm-2 control-label">名称</label>
+                <div class="col-sm-10">
+                  <input v-model="chapter.name" class="form-control" placeholder="名称">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">课程ID</label>
+                <div class="col-sm-10">
+                  <input v-model="chapter.courseId" class="form-control" placeholder="课程ID">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
 
 </template>
@@ -91,6 +127,7 @@ export default {
   name: "chapter",
   data: function (){
     return {
+      chapter:{},
       chapters: []
     }
   },
@@ -102,6 +139,10 @@ export default {
     // this.$parent.$parent.activeSidebar("business-chapter-sidebar");
   },
   methods:{
+    add(){
+      let _this = this;
+      $(".modal").modal("show");
+    },
     list(page){
       let _this = this;
 
@@ -112,6 +153,13 @@ export default {
         console.log("查询大章列表结果:",response);
         _this.chapters = response.data.list;
         _this.$refs.pagination.render(page,response.data.total);
+      })
+    },
+
+    save(page){
+      let _this = this;
+      _this.$axios.post('http://127.0.0.1:9000/business/admin/chapter/save',_this.chapter).then((response)=>{
+        console.log("保存大章列表结果:",response);
       })
     }
   }
--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,5 +1,6 @@
 package com.course.business.controller.admin;
 
+import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
 
@@ -25,5 +26,12 @@ public class ChapterController {
         LOG.info("pageDto:{}",pageDto);
         chapterService.list(pageDto);
         return pageDto;
+    }
+
+    @RequestMapping("/save")
+    public ChapterDto save(@RequestBody ChapterDto chapterDto){
+        LOG.info("chapterDto:{}",chapterDto);
+        chapterService.save(chapterDto);
+        return chapterDto;
     }
 }

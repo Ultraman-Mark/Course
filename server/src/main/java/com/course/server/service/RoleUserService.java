5cc41b2d25bf09b02db582eb7ac06e4ca8202726
Pengyunlu <2278625687@qq.com>
2022-04-21 13:37:59 +0800

    12-2-6.点击【关联用户】按钮时,加载所有用户

---
+++ server/src/main/java/com/course/server/service/RoleUserService.java
@@ -0,0 +1,73 @@
+package com.course.server.service;
+
+import com.course.server.domain.RoleUser;
+import com.course.server.domain.RoleUserExample;
+import com.course.server.dto.RoleUserDto;
+import com.course.server.dto.PageDto;
+import com.course.server.mapper.RoleUserMapper;
+import com.course.server.util.CopyUtil;
+import com.course.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoleUserService {
+
+    @Resource
+    private RoleUserMapper roleUserMapper;
+
+    /**
+    * 列表查询
+    */
+    public void list(PageDto pageDto){
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        RoleUserExample roleUserExample = new RoleUserExample();
+        List<RoleUser> roleUserList = roleUserMapper.selectByExample(roleUserExample);
+        PageInfo<RoleUser> pageInfo = new PageInfo<>(roleUserList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<RoleUserDto> roleUserDtoList = CopyUtil.copyList(roleUserList,RoleUserDto.class);
+        pageDto.setList(roleUserDtoList);
+    }
+
+    /**
+    * 保存
+    */
+    public void save(RoleUserDto roleUserDto){
+        RoleUser roleUser = CopyUtil.copy(roleUserDto,RoleUser.class);
+        if (!StringUtils.hasText(roleUserDto.getId())){
+            this.insert(roleUser);
+        }
+        else {
+            this.update(roleUser);
+        }
+    }
+
+    /**
+     * 插入
+     */
+    private void insert(RoleUser roleUser){
+        roleUser.setId(UuidUtil.getShortUuid());
+        roleUserMapper.insert(roleUser);
+    }
+
+    /**
+     * 更新
+     */
+    private void update(RoleUser roleUser) {
+        roleUserMapper.updateByPrimaryKey(roleUser);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id) {
+        roleUserMapper.deleteByPrimaryKey(id);
+    }
+}

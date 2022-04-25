a26abe8ccb06da02164e97b2a4680b62eabbfa4e
Pengyunlu <2278625687@qq.com>
2022-04-25 17:59:07 +0800

    13-2.集成boostrap模板,将头部和尾部分类

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Back to top 1</a>
+      </p>
+      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+      <p>New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'theFooter',
+  }
+</script>
